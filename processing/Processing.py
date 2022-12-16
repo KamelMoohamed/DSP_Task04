e1ef0e4c1be8035c246a32bf73bb325e3a2a9e16
youssef Shaban <85808789+youssef-shaban@users.noreply.github.com>
2022-12-16 21:56:16 +0200

    Uploading image from backend

--- processing/Processing.py
+++ processing/Processing.py
@@ -1,10 +1,30 @@
 import cv2 as cv
 import numpy as np
+import os
+from .imageClass import Image
+class ImageEditor:
+    def __init__(self) -> None:
+        self.img=[Image(),Image()]
+    
+    def upload_img(self:object,path:str,image_id:int):
+        dir_path=os.path.dirname(path)
+        
+        self.img[image_id].path=path
+        self.img[image_id].img_data = cv.imread(path,0)
+        self.img[image_id].imgfft = np.fft.fft2(self.img[image_id].img_data)
+        self.img[image_id].fshift = np.fft.fftshift(self.img[image_id].imgfft)
+        self.img[image_id].magnitude_spectrum = 20*np.log(np.abs(self.img[image_id].fshift))
+        self.img[image_id].phase_spectrum= np.angle(self.img[image_id].fshift)
 
-class Processing:
-    def convert_to_fourier(self, path):
-        self.img = cv.imread(path, 0)
-        self.dft = cv.dft(np.float32(self.img),flags = cv.DFT_COMPLEX_OUTPUT)
-        self.dft_shift = np.fft.fftshift(self.dft)
-        self.magnitude_spectrum = 20*np.log(cv.magnitude(self.dft_shift[:,:,0], self.dft_shift[:,:,1]))
-        return self.magnitude_spectrum
+        mag_path= os.path.join(dir_path,f"mag{image_id}.jpg")
+        phase_path=os.path.join(dir_path,f"phase{image_id}.jpg")
+
+        cv.imwrite(mag_path,self.scale(self.img[image_id].magnitude_spectrum))
+        cv.imwrite(phase_path,self.scale(self.img[image_id].phase_spectrum))
+
+        return [mag_path,phase_path]
+    
+
+    def scale(self,image_array):
+        image=((image_array - image_array.min()) * (1/(image_array.max() - image_array.min()) * 255)).astype('uint8')
+        return image
--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,12 @@
-let uploadImage = document.getElementsByClassName("upload-img")
+let uploadImage = document.getElementsByClassName("upload-img");
 let firstImg = document.getElementById("img1");
 firstImg.addEventListener("change", (e) => {
   sendImage();
   uploadFile("img1");
   setTimeout(makeGray.bind(null, "first-img-canvas"), 90);
-  for(i=0; i<uploadImage.length; i++){
-    if(uploadImage[i].classList[1]==firstImg.classList[0]){
-      uploadImage[i].classList.add("hide-frame")
+  for (i = 0; i < uploadImage.length; i++) {
+    if (uploadImage[i].classList[1] == firstImg.classList[0]) {
+      uploadImage[i].classList.add("hide-frame");
     }
   }
 });
@@ -15,46 +15,55 @@ secondImg.addEventListener("change", (e) => {
   sendImage();
   uploadFile("img2");
   setTimeout(makeGray.bind(null, "second-img-canvas"), 90);
-  for(i=0; i<uploadImage.length; i++){
-    if(uploadImage[i].classList[1]==secondImg.classList[0]){
-      uploadImage[i].classList.add("hide-frame")
-    }secondImg
+  for (i = 0; i < uploadImage.length; i++) {
+    if (uploadImage[i].classList[1] == secondImg.classList[0]) {
+      uploadImage[i].classList.add("hide-frame");
+    }
+    secondImg;
   }
 });
 
 var gray_image;
 
-function uploadFile(e){
-  file=document.getElementById(e);
-  gray_image = new SimpleImage(file); 
+function uploadFile(e) {
+  file = document.getElementById(e);
+  gray_image = new SimpleImage(file);
 }
 
 function makeGray(e) {
-for (var pixel of gray_image.values()){
-    var red= pixel.getRed();
-    var green= pixel.getGreen();
-    var blue= pixel.getBlue();
-    average= (red+green+blue)/3;
-     pixel.setRed(average);
-     pixel.setGreen(average);
-     pixel.setBlue(average);
-} 
-var canvas=document.getElementById(e);
-gray_image.drawTo(canvas);
+  for (var pixel of gray_image.values()) {
+    var red = pixel.getRed();
+    var green = pixel.getGreen();
+    var blue = pixel.getBlue();
+    average = (red + green + blue) / 3;
+    pixel.setRed(average);
+    pixel.setGreen(average);
+    pixel.setBlue(average);
+  }
+  var canvas = document.getElementById(e);
+  gray_image.drawTo(canvas);
 }
 
-let firstCanvas = document.getElementById("first-img-canvas")
-firstImg.addEventListener("mouseover",()=>{firstCanvas.classList.toggle("reduce-opacity")})
-firstImg.addEventListener("mouseout",()=>{firstCanvas.classList.toggle("reduce-opacity")})
-let secondCanvas = document.getElementById("second-img-canvas")
-secondImg.addEventListener("mouseover",()=>{secondCanvas.classList.toggle("reduce-opacity")})
-secondImg.addEventListener("mouseout",()=>{secondCanvas.classList.toggle("reduce-opacity")})
+let firstCanvas = document.getElementById("first-img-canvas");
+firstImg.addEventListener("mouseover", () => {
+  firstCanvas.classList.toggle("reduce-opacity");
+});
+firstImg.addEventListener("mouseout", () => {
+  firstCanvas.classList.toggle("reduce-opacity");
+});
+let secondCanvas = document.getElementById("second-img-canvas");
+secondImg.addEventListener("mouseover", () => {
+  secondCanvas.classList.toggle("reduce-opacity");
+});
+secondImg.addEventListener("mouseout", () => {
+  secondCanvas.classList.toggle("reduce-opacity");
+});
 function sendImage() {
   var formData = new FormData($("#upload-form")[0]);
 
   $.ajax({
     type: "POST",
-    url: "/upload-image",
+    url: "/upload-image/0",
     data: formData,
     contentType: false,
     cache: false,
@@ -65,4 +74,3 @@ function sendImage() {
     },
   });
 }
-
